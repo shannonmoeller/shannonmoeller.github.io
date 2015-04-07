350ab24427cd56bb5856de991ae2d8a450a16e92
Shannon Moeller <me@shannonmoeller.com>
2015-04-07 11:52:17 -0400

    Update index.html
    boilerplate
    
    fixed logo boilerplate

---
+++ tasks/dest.js
@@ -0,0 +1,47 @@
+import cli from './util/cli';
+import env from './util/env';
+import fs from './util/fs';
+import plug from './util/plug';
+import gulp from 'gulp';
+import del from 'del';
+
+let isServing = false,
+	isWatching = false,
+	paths = {
+		dest: env.getDestPath(),
+		deploy: env.getDestPath('**/{*,.*}'),
+		watch: env.getDestPath('**')
+	};
+
+gulp.task('dest:clean', function () {
+	del.sync(paths.dest);
+});
+
+gulp.task('dest:deploy', function () {
+	return fs
+		.src(paths.deploy)
+		.pipe(plug.ghPages({
+			branch: 'master'
+		}));
+});
+
+gulp.task('dest:serve', function () {
+	if (!isServing) {
+		isServing = true;
+
+		plug.connect
+			.server({
+				livereload: cli.watch,
+				port: cli.port,
+				root: paths.dest
+			});
+	}
+
+	if (!isWatching && cli.watch) {
+		isWatching = true;
+
+		plug
+			.watch(paths.watch)
+			.pipe(plug.connect.reload());
+	}
+});
