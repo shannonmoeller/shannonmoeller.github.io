129ef313b26bb5e39a658d47faf05de38c49614d
Shannon Moeller <smoeller@Minnie.home>
2015-10-08 11:03:10 -0400

    boilerplate

--- README.md
+++
@@ -1,1 +0,0 @@
-# shannonmoeller.github.io
---
+++ src/assets/scripts/main.js
@@ -0,0 +1,7 @@
+// Polyfills
+import svg4everybody from 'svg4everybody/dist/svg4everybody';
+
+// Elements
+import '../elements/base-element/element';
+
+svg4everybody();
